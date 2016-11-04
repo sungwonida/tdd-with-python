a6cc2b1314f49d7c545713b8cf606baae15d5fd0
swjung <swjung@canvasbio.com>
2016-11-04 16:29:45 +0900

    Added a helper file (shortcut)

---
+++ shortcuts.el
@@ -0,0 +1,14 @@
+(defun tdd-functional_tests ()
+  "tdd-functional_tests"
+  (interactive)
+  (message "Test-Driven Development with Python: Checking Functional Tests..")
+  (compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 functional_tests.py"))
+
+(defun tdd-unit_tests ()
+  "tdd-unit_tests"
+  (interactive)
+  (message "Test-Driven Development with Python: Checking Unit Tests..")
+  (compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 manage.py test"))
+
+(define-key global-map [(f7)] 'tdd-functional_tests)
+(define-key global-map [(f8)] 'tdd-unit_tests)

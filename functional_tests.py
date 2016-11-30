8b333d9bca11c858185b6d339ab246778c0a08d5
David Jung <Kirin@Davidui-MacBook-Pro.local>
2016-11-30 23:00:45 +0900

    Basic view now return minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
--- shortcuts.el
+++ shortcuts.el
@@ -2,13 +2,15 @@
   "tdd-functional_tests"
   (interactive)
   (message "Test-Driven Development with Python: Checking Functional Tests..")
-  (compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 functional_tests.py"))
+  ;(compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 functional_tests.py"))
+  (compile "cd /Users/Kirin/Development/tdd-with-python && python3 functional_tests.py"))
 
 (defun tdd-unit_tests ()
   "tdd-unit_tests"
   (interactive)
   (message "Test-Driven Development with Python: Checking Unit Tests..")
-  (compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 manage.py test"))
+  ;(compile "cd /home/swjung/Development/tdd-with-python/superlists && python3 manage.py test"))
+  (compile "cd /Users/Kirin/Development/tdd-with-python && python3 manage.py test"))
 
 (define-key global-map [(f7)] 'tdd-functional_tests)
 (define-key global-map [(f8)] 'tdd-unit_tests)
